e8c6323e5482e67746975602c19551553adacd01
wakababa <altgblbl@gmail.com>
2022-11-29 23:30:57 +0300

    index.tsx changed

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,137 +1,101 @@
-import { Link } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { useState } from "react";
 
-import { useOptionalUser } from "~/utils";
+export async function gerHeroes() {
+  const res = await fetch(`https://www.dota2.com/datafeed/herolist?language=english`).then((res) => res.json());
+  return res.result.data.heroes;
+}
 
-export default function Index() {
-  const user = useOptionalUser();
+
+type LoaderData = {
+  heroes: Awaited<ReturnType<typeof gerHeroes>>;
+};
+export const loader = async () => {
+  return json<LoaderData>({
+    heroes: await gerHeroes()
+  });
+};
+
+type HeroProps = {
+  id: Number
+  name: String
+  name_loc: String
+  name_english_loc: String
+  primary_attr: keyof typeof Primary_attrEnum,
+  complexity: Number
+}
+
+enum Primary_attrEnum {
+  Strength,
+  Agility,
+  Intelegent
+}
+
+const Hero = ({ hero }: { hero: HeroProps }) => {
+  const {id, name, name_english_loc, primary_attr } = hero;
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <div className="relative sm:pb-16 sm:pt-8">
-        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
-            <div className="absolute inset-0">
-              <img
-                className="h-full w-full object-cover"
-                src="https://user-images.githubusercontent.com/1500684/158276320-c46b661b-8eff-4a4d-82c6-cf296c987a12.jpg"
-                alt="BB King playing blues on his Les Paul guitar"
-              />
-              <div className="absolute inset-0 bg-[color:rgba(27,167,254,0.5)] mix-blend-multiply" />
-            </div>
-            <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
-              <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
-                <span className="block uppercase text-blue-500 drop-shadow-md">
-                  Blues Stack
-                </span>
-              </h1>
-              <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
-                Check the README.md file for instructions on how to get this
-                project deployed.
-              </p>
-              <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
-                {user ? (
-                  <Link
-                    to="/notes"
-                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
-                  >
-                    View Notes for {user.email}
-                  </Link>
-                ) : (
-                  <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-                    <Link
-                      to="/join"
-                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
-                    >
-                      Sign up
-                    </Link>
-                    <Link
-                      to="/login"
-                      className="flex items-center justify-center rounded-md bg-blue-500 px-4 py-3 font-medium text-white hover:bg-blue-600"
-                    >
-                      Log In
-                    </Link>
-                  </div>
-                )}
-              </div>
-              <a href="https://remix.run">
-                <img
-                  src="https://user-images.githubusercontent.com/1500684/158298926-e45dafff-3544-4b69-96d6-d3bcc33fc76a.svg"
-                  alt="Remix"
-                  className="mx-auto mt-16 w-full max-w-[12rem] md:max-w-[16rem]"
-                />
-              </a>
-            </div>
-          </div>
-        </div>
+    <a style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-start",
+      justifyContent: "flex-end",
+      backgroundImage: `url("https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/${name.split("npc_dota_hero_")[1]}.png")`,
+      backgroundSize: "cover",
+      left: "calc(122px + (2 * (100% - 225px)) / 4)",
+      top: "calc(284px)",
+      width: 225,
+      height: 127,
+      textDecoration:"none"
+    }}
+      href={`heroes/${id}`}
+    >
+      <h6 style={{
+        marginTop: "auto",
+        background: "white",
+        padding: 10,
+        margin: 0,
+        borderTopRightRadius: 20
+      }}>{name_english_loc}</h6>
+    </a>
+  );
+};
 
-        <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8">
-          <div className="mt-6 flex flex-wrap justify-center gap-8">
-            {[
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157764397-ccd8ea10-b8aa-4772-a99b-35de937319e1.svg",
-                alt: "Fly.io",
-                href: "https://fly.io",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/158238105-e7279a0c-1640-40db-86b0-3d3a10aab824.svg",
-                alt: "PostgreSQL",
-                href: "https://www.postgresql.org/",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157764484-ad64a21a-d7fb-47e3-8669-ec046da20c1f.svg",
-                alt: "Prisma",
-                href: "https://prisma.io",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157764276-a516a239-e377-4a20-b44a-0ac7b65c8c14.svg",
-                alt: "Tailwind",
-                href: "https://tailwindcss.com",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157764454-48ac8c71-a2a9-4b5e-b19c-edef8b8953d6.svg",
-                alt: "Cypress",
-                href: "https://www.cypress.io",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157772386-75444196-0604-4340-af28-53b236faa182.svg",
-                alt: "MSW",
-                href: "https://mswjs.io",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157772447-00fccdce-9d12-46a3-8bb4-fac612cdc949.svg",
-                alt: "Vitest",
-                href: "https://vitest.dev",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157772662-92b0dd3a-453f-4d18-b8be-9fa6efde52cf.png",
-                alt: "Testing Library",
-                href: "https://testing-library.com",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157772934-ce0a943d-e9d0-40f8-97f3-f464c0811643.svg",
-                alt: "Prettier",
-                href: "https://prettier.io",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157772990-3968ff7c-b551-4c55-a25c-046a32709a8e.svg",
-                alt: "ESLint",
-                href: "https://eslint.org",
-              },
-              {
-                src: "/heroes",
-                alt: "Heroes",
-              },
-            ].map((img) => (
-              <a
-                key={img.href}
-                href={img.href}
-                className="flex h-16 w-32 justify-center p-1 grayscale transition hover:grayscale-0 focus:grayscale-0"
-              >
-                <img alt={img.alt} src={img.src} className="object-contain" />
-              </a>
-            ))}
-          </div>
-        </div>
+const SearchHeroes = ({ onHandleChange }: { onHandleChange: Function }) => {
+  return (
+    <input style={{ padding: 15, margin: 5 }} placeholder={"Search"} type={"text"}
+           onChange={(e) => onHandleChange(e.target.value)} />
+  );
+};
+
+export default function Heroes() {
+  const { heroes } = useLoaderData() as LoaderData;
+  const [search, setSearch] = useState<String>("");
+  return (
+    <div style={{
+      display: "flex",
+      flexDirection: "column", alignItems: "center",
+      backgroundImage: "url('https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/backgrounds/featured.jpg')",
+      padding: 10,
+      height: "100%",
+      backgroundPosition: "center",
+      backgroundRepeat:"no-repeat",
+      backgroundSize: "cover"
+    }}>
+      <SearchHeroes onHandleChange={(val: String) => setSearch(val)} />
+      <div style={{
+        fontFamily: "system-ui, sans-serif",
+        lineHeight: "1.4",
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        width: 700
+      }}>
+        {heroes.filter((hero: HeroProps) => hero.name_english_loc.toLocaleLowerCase().includes(String(search))).map((hero: HeroProps) =>
+          <Hero hero={hero} />)}
       </div>
-    </main>
+    </div>
   );
 }
+
+
